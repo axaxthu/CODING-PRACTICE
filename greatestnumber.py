fa7fd30daaa5f326de73ae54826b755d7bbeac26
ANANTHAPADMANABHAN <ananthasayanam2008@gmail.com>
2022-10-09 16:41:18 +0530

    > number amoung 2

---
+++ greatestnumber.py
@@ -0,0 +1,7 @@
+n1= int(input("input your first number: "))
+n2= int(input("input your second number: "))
+
+if n1>n2:
+    print("greatest number is ", n1)
+else:   
+    print("greatest number is ", n2) 
