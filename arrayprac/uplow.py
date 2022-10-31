d2b1aee12010c4f1c7971e5a61e517ad0c519062
ANANTHAPADMANABHAN <ananthasayanam2008@gmail.com>
2022-10-31 20:57:57 +0530

    CASE CHANGING

---
+++ arrayprac/uplow.py
@@ -0,0 +1,4 @@
+string="Harpic lover"
+print(string.lower()) #changes the string to smaller case
+print(string.upper()) #changes the string to upper case\
+print(string.capitalize()) #Makes the first letter of the string to capital
