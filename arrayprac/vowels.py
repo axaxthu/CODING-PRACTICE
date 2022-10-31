19b3321de4254ced62db2bb8de1296f1b189ec09
ANANTHAPADMANABHAN <ananthasayanam2008@gmail.com>
2022-10-31 20:58:35 +0530

    VOWEL

---
+++ arrayprac/vowels.py
@@ -0,0 +1,15 @@
+vow=str(input("ENTER A VOWEL: "))
+
+if "a" in vow  or "A" in vow:
+    print("it is a vowel")
+elif "e" in vow or "E" in vow :
+    print("it is a vowel")
+elif "i"in vow or "I" in vow:
+    print("it is a vowel")
+elif "o" in vow or "O" in vow:
+    print("it is a vowel")
+elif "u" in vow or "U" in vow:
+    print("it is a vowel")
+else:
+    print("IT IS NOT A VOWEL")        
+
