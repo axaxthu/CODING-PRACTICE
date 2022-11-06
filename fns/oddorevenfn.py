48003def1daa9d68274a4835a53f312c9fe6d871
ANANTHAPADMANABHAN <ananthasayanam2008@gmail.com>
2022-11-06 23:37:27 +0530

    ODD OR EVEN FN.

---
+++ fns/oddorevenfn.py
@@ -0,0 +1,17 @@
+#define a function to print the number of odd and even nos in a list
+
+def count(listofnum):
+    even=0
+    odd=0
+    for i in listofnum:
+        if i%2==0:
+            even+=1
+        else:
+            odd+=1  
+    return even,odd      
+
+
+listofnum=[32,21,64,100,13]
+even,odd=count(listofnum)
+print(even)
+print(odd)
