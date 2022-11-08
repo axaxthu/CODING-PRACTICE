860504b27899c69fbb448ce591689cc16c52b7c2
ANANTHAPADMANABHAN <ananthasayanam2008@gmail.com>
2022-11-08 21:52:22 +0530

    NAME LAST NAME AGE

---
+++ fns/eve.py
@@ -0,0 +1,6 @@
+#define a fn to print a person's name,last name and age
+
+def eve(name,lname,age):
+    print("My name is {} {} aged {}".format(name,lname,age)) #int age cannot be concatenated
+
+eve("ANANTHU","S",23)    
