1f6920b71210e754f8ff14b3723fbfda160d3cad
ANANTHAPADMANABHAN <ananthasayanam2008@gmail.com>
2022-11-08 21:53:08 +0530

    NEEEWWWWW

---
+++ fns/new.py
@@ -0,0 +1,7 @@
+#create a function that will take two arguments name and age and print their values
+
+def new(name,age):
+    print("MY NAME IS ",name,", MY AGE IS",age)
+    
+
+new("ANANTHU",18)    
