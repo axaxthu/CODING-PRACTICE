23f3fa40a212ba907a7a1c5eb0de93655b232d06
ANANTHAPADMANABHAN <ananthasayanam2008@gmail.com>
2022-11-09 22:12:49 +0530

    LAMBDA

---
+++ fns/lambda.py
@@ -0,0 +1,4 @@
+# function used as a shortcut: this is lambda 
+x=lambda a:a*a #recheck sqrfn.py (in that program a function is used to find the square) #here lambda fn is used instead which is a shortcut
+ans=x(5)
+print(ans)
