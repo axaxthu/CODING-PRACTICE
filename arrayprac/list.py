c703a358ec898014ad9f4848b43f14a052ff26ca
ANANTHAPADMANABHAN SS <ananthasayanam2008@gmail.com>
2023-01-04 18:44:59 +0530

    PYTHON

---
+++ arrayprac/list.py
@@ -0,0 +1,11 @@
+#change colours black and blue to green and purple
+colors=["RED","BLACK","BLUE","PINK"]
+colors[1:3]=["GREEN","PURPLE"]
+print(colors)
+
+#list notes:
+#stores multiple values in one variable
+#non homogenous
+#ordered
+#changable/mutable
+#allows duplicate values
