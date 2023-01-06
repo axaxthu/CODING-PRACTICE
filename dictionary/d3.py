3665e5d7f1a8a952ea3e9ffdb335b22e07d78110
ANANTHAPADMANABHAN SS <ananthasayanam2008@gmail.com>
2023-01-06 21:47:14 +0530

    PYTHON

---
+++ dictionary/d3.py
@@ -0,0 +1,14 @@
+#W.A.P TO PRINT THE PYTHON MARKS
+myDictionary = {
+    "class":{
+        "student":{
+            "name" : "abc",
+            "marks" : {
+                "python" : 90,
+                "web" : 95
+            }
+        }
+    }
+}
+print(myDictionary["class"]["student"]["marks"]["python"])
+
