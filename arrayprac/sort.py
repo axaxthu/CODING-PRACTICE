ee3a45e9bc61c16d2ed68c4da5d81b02bb64c099
ANANTHAPADMANABHAN SS <ananthasayanam2008@gmail.com>
2023-01-06 21:43:31 +0530

    PYTHON

---
+++ arrayprac/sort.py
@@ -0,0 +1,13 @@
+colors=["BLUE","YELLOW","GREEN","PINK","BLACK"]
+colors.sort() #sort the elements in the list
+print(colors)
+colors.sort(reverse=True) #sort in reverse order(descending order)
+print(colors)
+
+#wap to turn every item of list into its square
+
+s=[1,2,3,4,5]
+new=[]
+for i in s:
+    new.append(i*i)
+print(new)    
