06d7bf4c666db17c3e01c9f7244cf35f6e83ca7f
ANANTHAPADMANABHAN SS <ananthasayanam2008@gmail.com>
2023-01-06 21:50:24 +0530

    PYTHON

---
+++ sets/s2.py
@@ -0,0 +1,6 @@
+myset={"car","boat","train","bike","tractor"} 
+myset2={1,2,3}
+myset.discard("bike")
+myset.add("cycle")
+print(myset)
+#for say if I add "car" which is already present it wont be added cuz car is already present[sets do not allow duplicate elements 
